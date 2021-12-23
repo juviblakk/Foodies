4e1dc7b048f6f5a5dcb9e72b3e86c2a9afc70f64
juviblakk <92520371+juviblakk@users.noreply.github.com>
2021-12-22 22:50:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once 'includes/header.php';
     <div class="content">
         <h3>Welcome to Foodie's great food with a smile</h3>
         <p>An experience in caribbean elegance and flavor</p>
-        <a href="menu.php"><button class="btn">Our Menu</button></a>
+        <a href="Menu.php"><button class="btn">Our Menu</button></a>
         <a href="order.php"><button class="btn">Order</Table></button></a>
     </div>
 
